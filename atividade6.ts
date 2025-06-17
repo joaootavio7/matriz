99d8adf26237910728afe9c3917e01da0abc50d7
João Otávo <ojoao21513@gmail.com>
2025-06-17 16:45:47 -0300

    matriz

--- atividade6.ts
+++ atividade6.ts
@@ -2,20 +2,20 @@
 aceitando somente números de 0 a 9. Em seguida,
 exiba a matriz na tela.*/
 
-let teclado = require (`prompt-sync`)();
-let matrizDois: number[][] = new Array (10);
-for(let i = 0; i <3; i++){
-    matrizDois[i] = new Array (3);  
+let teclado = require(`prompt-sync`)();
+let matrizDois: number[][] = new Array(10);
+for (let i = 0; i < 3; i++) {
+    matrizDois[i] = new Array(3);
 
     for (let j = 0; j < 3; j++) {
-        let numero: number = teclado(`digite  o nome que vai estar no seu endereço [${i}, ${j}]`)
-        if(numero <= 9){
-            matrizDois[i][j]= numero       
+        let numero: number = teclado(`digite  o número que vai estar no seu endereço [${i}, ${j}]`)
+        if (numero <= 9) {
+            matrizDois[i][j] = numero
         }
-        else{
+        else {
             j--;
             console.log("erro");
-            
+           
         }
     }
 }
